c5568c61c061654627ab3dafbab4fcc65628339b
Anna <filatovaa@yahoo.com>
2020-08-19 00:13:23 -0700

    updated the like and dislike functionality

--- src/App.js
+++ src/App.js
@@ -10,15 +10,44 @@ class App extends React.Component{
       //our intial state is empty
       products:[],
     }
+    this.handleDislike=this.handleDislike.bind(this)
+    this.handleLike=this.handleLike.bind(this)
+   
     
   }
   //setting an instance of state populating with data 
   componentDidMount(){
     this.setState({products:products})
   }
+
+  handleDislike(id){
+    // console.log('this is the id ;) ' +id)
+    let updatedProducts= this.state.products.map(item=>{
+      if(item.id===id){
+        return Object.assign({}, item, {
+          votes: item.votes-1
+        })
+      } else{
+        return item
+      }
+    })
+    this.setState({products: updatedProducts})
+  }
   
+  handleLike(id){
+    // console.log(` this is me likeing `+id)
+    let updatedProducts= this.state.products.map(item=>{
+      if(item.id===id){
+        return Object.assign({}, item, {
+          votes:item.votes+1
+        })
+      } else { return item}
+    })
+      this.setState({products: updatedProducts})
+  }
 
   render(){
+  
     console.log(this.state.products)
     let product= this.state.products.map(product=>(
       <Individual
@@ -29,14 +58,16 @@ class App extends React.Component{
       url={product.urs}
       votes={product.votes}
       submitterAvatarUrl={product.submitterAvatarUrl}
-
+      dislike={this.handleDislike}
+      like={this.handleLike}
+     
        />
 
     ))
     return(
-      <div class='main-container'>
+      <div className='main-container'>
         <h1>MY LIST</h1>
-        <div>{product}</div>
+        <div className='cards'>{product}</div>
       </div>
      
     )
@@ -46,11 +77,18 @@ class App extends React.Component{
 class Individual extends React.Component{
   constructor(props){
     super(props)
-    
-  }
-  handleUpvote(){
 
+    this.buttonDislike=this.buttonDislike.bind(this)
+    this.buttonLike=this.buttonLike.bind(this)
   }
+  
+buttonDislike(){
+  this.props.dislike(this.props.id)
+}
+buttonLike(){
+  this.props.like(this.props.id)
+}
+  
   render(){
     
     return(
@@ -64,8 +102,8 @@ class Individual extends React.Component{
           <a>{this.props.url}</a>
           <h5 className='description'>{this.props.description}</h5>
           <div className='votes'>
-          <button className='like-button'></button>
-          <button className='dislike-button'></button>
+          <button  onClick={this.buttonLike} className='like-button' />
+          <button onClick={this.buttonDislike} className='dislike-button'></button>
           {this.props.votes}
         </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -19,7 +19,43 @@ const products = [
         votes: generateVoteCount(),
         submitterAvatarUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
         
-    }
+    },
+    {
+        id: 3,
+        title: 'Love Dogs',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
+        
+      },
+      {
+          id: 4,
+          title: 'Yellow Pail',
+          description: 'On-demand sand castle construction expertise.',
+          url: '#',
+          votes: generateVoteCount(),
+          submitterAvatarUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
+          
+      },
+      {
+        id: 5,
+        title: 'Love Dogs',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
+        
+      },
+      {
+          id: 6,
+          title: 'Yellow Pail',
+          description: 'On-demand sand castle construction expertise.',
+          url: '#',
+          votes: generateVoteCount(),
+          submitterAvatarUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
+          
+      }
 ]
 
 export default products
